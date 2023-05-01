3e68922daa5abf742bd9564c256ea9cb3cd78dcf
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-05-01 15:36:28 +0500

    ali

--- app/Http/Controllers/Api/VendorController.php
+++ app/Http/Controllers/Api/VendorController.php
@@ -13,7 +13,7 @@ class VendorController extends Controller
     {
     
         $vendor = Vendor::find($request->id);
-        $vendor->online = $request->online== 'online' ? true : false;
+        $vendor->online = $request->online;
         $vendor->save();
     
         return Api::setResponse('Vendor', $vendor);

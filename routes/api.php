3e2b378469cb538a9d9698ca0a1c2cc9ad8a428d
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-05-24 12:43:04 +0500

    ali

--- routes/api.php
+++ routes/api.php
@@ -40,8 +40,9 @@ Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
         Route::any('service/store', [ServiceController::class, 'store']);
         Route::any('vendor/online', [VendorController::class, 'offline']);
         Route::any('vendor/update', [VendorController::class, 'edit']);
-        Route::any('balance/add', [VendorController::class, 'addbalance']);
+     
     });
     Route::group(['middleware' => 'auth:api'], function () {
+        Route::any('balance/add', [VendorController::class, 'addbalance']);
     });
 });

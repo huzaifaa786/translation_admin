a5f6d7b5948b4940f039e20025275c8807876e23
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-01 12:05:31 +0500

    ali

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -16,6 +16,6 @@ class ReportController extends Controller
 
    public function weeklysale(Request $request){
     $days = Report::weaklySale($request->week);
-    return Api::setResponse('day', $days);
+    return Api::setResponse('days', $days);
 }
 }

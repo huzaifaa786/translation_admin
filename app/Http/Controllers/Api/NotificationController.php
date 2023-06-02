bb333cdd049e3a710eb0bb6e19dd55ead211c2f2
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-02 12:46:40 +0500

    ali

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -6,6 +6,7 @@ use App\Helpers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {

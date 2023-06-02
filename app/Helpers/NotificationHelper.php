7a2ede59a1df9ca3e14c5ea21a3675c95c228109
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-02 15:51:56 +0500

    ali

--- app/Helpers/NotificationHelper.php
+++ app/Helpers/NotificationHelper.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Log;
 class NotificationHelper
 {
 
-    public static function send($object,$token)
+    public static function send($object, $token)
     {
-
+        dd($token);
         $notification['to'] = $token;
         $notification['notification']['click_action'] = "FLUTTER_NOTIFICATION_CLICK";
         $notification['notification']['title'] = $object->title;
@@ -22,9 +22,9 @@ class NotificationHelper
             ->withHeaders(["authorization" => "key=AAAAomo3OJ0:APA91bF2-moezCzjjdSJSNCiw0kfh4xrWIK3uaxrw23UBX0Yp7hi01g_dh6v1cgMnWO-Rjni_TSzDbTBN_E0EzXUSl9CHI-TRkXO0WQgQe8s-glBE0YP3Ksz1J4Tm0ZeSbURlK_vGsQC"])
             ->post("https://fcm.googleapis.com/fcm/send");
 
-            Log::alert($result);
+        Log::alert($result);
     }
-    public static function vendor($object,$vendor)
+    public static function vendor($object, $vendor)
     {
 
         $notification['to'] = $vendor;
@@ -35,9 +35,9 @@ class NotificationHelper
         $notification['data'] = $object->extra;
 
         $result = Http::withOptions(['json' => $notification])
-        ->withHeaders(["authorization" => "key=AAAAqimwxso:APA91bFINzYvYnYCpVuD8SqkgBzwEd0QoKk7ciR5qVreFaVf6c-YvbEaOMDOk8DhSEdeQ0TmbbT9XkZesiJ4dlpQ2D19O1XFMM-PQBnxrzMjmPKpjlw6vuYSPCMn-H1417-z--J5NdUQ"])
-        ->post("https://fcm.googleapis.com/fcm/send");
+            ->withHeaders(["authorization" => "key=AAAAqimwxso:APA91bFINzYvYnYCpVuD8SqkgBzwEd0QoKk7ciR5qVreFaVf6c-YvbEaOMDOk8DhSEdeQ0TmbbT9XkZesiJ4dlpQ2D19O1XFMM-PQBnxrzMjmPKpjlw6vuYSPCMn-H1417-z--J5NdUQ"])
+            ->post("https://fcm.googleapis.com/fcm/send");
 
-            Log::alert($result);
+        Log::alert($result);
     }
 }

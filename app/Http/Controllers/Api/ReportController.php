abed7d73d8c0dd3b9959580deb22e4d8592f27aa
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-02 10:32:36 +0500

    ali

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -6,10 +6,13 @@ use App\Helpers\Api;
 use App\Helpers\Report;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
     public function sales(Request $request){
+        $vendor= Auth::vendor()->id;
+        dd($vendor);
         $days = Report::MonthlySale($request->month,$request->year);
         return Api::setResponse('day', $days);
    }

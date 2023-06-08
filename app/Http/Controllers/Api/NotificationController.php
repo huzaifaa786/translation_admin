a6c5450931b2e0fa405e53330c63a46a9680e403
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-08 14:46:24 +0500

    ali

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -26,8 +26,8 @@ class NotificationController extends Controller
 
     public function check()
     {
-  dd(Auth::user()->id);
-        $has_new = Notification::where('vendor_id',Auth::user()->id)->where('is_read', false)->count();
+        dd(Auth::guard('vendor_api')->user()->id);
+        $has_new = Notification::where('vendor_id',Auth::guard('vendor_api')->user()->id)->where('is_read', false)->count();
         if($has_new > 0)
             return Api::setResponse('exist',true);
         return Api::setResponse('exist',false);

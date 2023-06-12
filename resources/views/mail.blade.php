e709a6f1032a8bcc08e73ef3c74843a5caa33596
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-12 14:35:15 +0500

    ali

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -14,7 +14,7 @@
         style="background-color: #ffffff; border-collapse: collapse;">
         <tr>
             <td align="center" style="padding: 40px 0;">
-                <img src="assets/img/logo2.png" alt="Logo" width="150"
+                <img src="https://https://translation.klickwash.net//assets/img/logo2.png" alt="Logo" width="150"
                     height="auto" style="display: block;">
             </td>
         </tr>

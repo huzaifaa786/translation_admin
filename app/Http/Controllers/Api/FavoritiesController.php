35fa1327a14eb9f3af1b64aee546b75559498aa8
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-16 16:32:48 +0500

    ali

--- app/Http/Controllers/Api/FavoritiesController.php
+++ app/Http/Controllers/Api/FavoritiesController.php
@@ -13,31 +13,31 @@ class FavoritiesController extends Controller
     public function store(Request $request)
     {
         $user_id = Auth::user()->id;
+        dd($user_id);
         $vendor_id = $request->input('vendor_id');
-        
+
         // Check if the favorite entry already exists
         $existingFavorite = Favorities::where('user_id', $user_id)
             ->where('vendor_id', $vendor_id)
             ->first();
-        
+
         if ($existingFavorite) {
-           $existingFavorite->delete();
-           return response()->json(['message' => 'vendor will be unFavorite']);
+            $existingFavorite->delete();
+            return response()->json(['message' => 'vendor will be unFavorite']);
         }
-        
+
         // Delete any existing favorite entry for the same user and vendor
         Favorities::where('user_id', $user_id)
             ->where('vendor_id', $vendor_id)
             ->delete();
-        
+
         // Create a new favorite entry
-        $data =Favorities::create([
+        $data = Favorities::create([
             'user_id' => $user_id,
             'vendor_id' => $vendor_id,
         ]);
-        
-      
+
+
         return Api::setResponse('favorities', $data);
     }
-    
-}    
+}

6ca44525a599a957ffa25e83f402e701d589e554
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-06-17 15:04:41 +0500

    ali

--- config/app.php
+++ config/app.php
@@ -182,6 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        \Chatify\ChatifyServiceProvider::class,
 
 
         /*
@@ -211,7 +212,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Image' => Intervention\Image\Facades\Image::class
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Chatify' => Chatify\Facades\ChatifyMessenger::class,
 
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),

fc894599f693ca2c7e56b594b88887637dbe361f
Huzaifa Aslam <tritechuzaifa@gmail.com>
2023-07-06 12:28:33 +0500

    ali

--- app/Http/Controllers/Api/FavoritiesController.php
+++ app/Http/Controllers/Api/FavoritiesController.php
@@ -45,11 +45,16 @@ class FavoritiesController extends Controller
 
     public function userCheck(Request $request)
     {
-        $check = Favorities::where('user_id', Auth::guard('api')->user()->id)->where('vendor_id', $request->vendor_id)->first();
-        if ($check > 0)
-            return Api::setResponse('favourit', false);
-        return Api::setResponse('favourit', true);
+        $user = Auth::guard('api')->user();
+        if ($user && $request->has('vendor_id')) {
+            $check = Favorities::where('user_id', $user->id)->where('vendor_id', $request->vendor_id)->exists();
+            if ($check) {
+                return Api::setResponse('favourit', true);
+            }
+        }
+        return Api::setResponse('favourit', false);
     }
+    
     public function getfavorities(Request $request)
     {
 

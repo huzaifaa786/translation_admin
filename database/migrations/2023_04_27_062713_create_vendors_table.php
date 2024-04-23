dee697a278608b91219758674e37c652d2dcb203
Mubeen Malik <mubeenmalik2829@gmail.com>
2024-04-23 13:57:04 +0500

    currency

--- database/migrations/2023_04_27_062713_create_vendors_table.php
+++ database/migrations/2023_04_27_062713_create_vendors_table.php
@@ -31,7 +31,6 @@ return new class extends Migration
             $table->string('firebase_token')->nullable();
             $table->json('language');
             $table->bigInteger('rating')->default(0.0);
-            $table->string('currency')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

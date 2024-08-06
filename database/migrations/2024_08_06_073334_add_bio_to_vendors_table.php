84dc4372e12c25bb83bcaee4878c95dd71fecfc2
huzaifa <ihuzaifaaslam@gmail.com>
2024-08-06 12:40:10 +0500

    adsf

--- database/migrations/2024_08_06_073334_add_bio_to_vendors_table.php
+++ database/migrations/2024_08_06_073334_add_bio_to_vendors_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('vendors', function (Blueprint $table) {
-            $table->text('bio');
+            $table->text('bio')->default('Translator with 10 years of experience');
         });
     }
 
